545236e91060fbafebb7a361f8e3d01883d83117
Daniel van der Wal <dvanderw@pip07.inf.fh-koeln.de>
2014-08-08 15:34:37 +0200

    bgr to rgb images

--- AVS_ImageCapturer/src/de/fh_koeln/avs/imagecapturer/converter/MatToBufferedImageConverter.java
+++ AVS_ImageCapturer/src/de/fh_koeln/avs/imagecapturer/converter/MatToBufferedImageConverter.java
@@ -1,6 +1,7 @@
 package de.fh_koeln.avs.imagecapturer.converter;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import org.opencv.core.Mat;
 
 /**
@@ -9,27 +10,20 @@ import org.opencv.core.Mat;
  */
 public final class MatToBufferedImageConverter {
 
-    private BufferedImage img;
-    private byte[] data;
-
-    public void getSpace(Mat imageMat) {
-        int w = imageMat.cols();
-        int h = imageMat.rows();
-        if (data == null || data.length != w * h * 3) {
-            data = new byte[w * h * 3];
-        }
-        if (img == null || img.getWidth() != w || img.getHeight() != h
-                || img.getType() != BufferedImage.TYPE_3BYTE_BGR) {
-            img = new BufferedImage(w, h,
-                    BufferedImage.TYPE_3BYTE_BGR);
-        }
-    }
-
     public BufferedImage convertToBufferedImage(Mat matImage) {
-        getSpace(matImage);
-        matImage.get(0, 0, data);
-        img.getRaster().setDataElements(0, 0, matImage.cols(), matImage.rows(),
-                data);
-        return img;
+        int type = BufferedImage.TYPE_BYTE_GRAY;
+        if (matImage.channels() > 1) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        int bufferSize
+                = matImage.channels() * matImage.cols() * matImage.rows();
+        byte[] b = new byte[bufferSize];
+        matImage.get(0, 0, b); // get all the pixels
+        BufferedImage image = new BufferedImage(matImage.cols(),
+                matImage.rows(), type);
+        final byte[] targetPixels = ((DataBufferByte) image.getRaster().
+                getDataBuffer()).getData();
+        System.arraycopy(b, 0, targetPixels, 0, b.length);
+        return image;
     }
 }
