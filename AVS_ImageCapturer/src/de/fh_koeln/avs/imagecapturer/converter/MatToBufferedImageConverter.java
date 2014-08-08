6c9daae42cbf7460ee9bcf532dd48e59b1e930a1
Daniel van der Wal <dvanderw@pip07.inf.fh-koeln.de>
2014-08-08 15:01:27 +0200

    Implemented MatToBufferedImageConverter

--- AVS_ImageCapturer/src/de/fh_koeln/avs/imagecapturer/converter/MatToBufferedImageConverter.java
+++ AVS_ImageCapturer/src/de/fh_koeln/avs/imagecapturer/converter/MatToBufferedImageConverter.java
@@ -7,9 +7,29 @@ import org.opencv.core.Mat;
  *
  * @author Daniel van der Wal
  */
-public class MatToBufferedImageConverter {
+public final class MatToBufferedImageConverter {
+
+    private BufferedImage img;
+    private byte[] data;
+
+    public void getSpace(Mat imageMat) {
+        int w = imageMat.cols();
+        int h = imageMat.rows();
+        if (data == null || data.length != w * h * 3) {
+            data = new byte[w * h * 3];
+        }
+        if (img == null || img.getWidth() != w || img.getHeight() != h
+                || img.getType() != BufferedImage.TYPE_3BYTE_BGR) {
+            img = new BufferedImage(w, h,
+                    BufferedImage.TYPE_3BYTE_BGR);
+        }
+    }
 
     public BufferedImage convertToBufferedImage(Mat matImage) {
-        return null;
+        getSpace(matImage);
+        matImage.get(0, 0, data);
+        img.getRaster().setDataElements(0, 0, matImage.cols(), matImage.rows(),
+                data);
+        return img;
     }
 }
