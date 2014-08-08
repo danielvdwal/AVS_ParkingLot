ba4fa494ac3a6a2628993f6eb6b5d87923f54f84
Daniel van der Wal <dvanderw@pip07.inf.fh-koeln.de>
2014-08-08 14:51:58 +0200

    Implemented ImageCpaturerController Added MatToBufferedImageConvereter (returns null atm)

--- AVS_ImageCapturer/src/de/fh_koeln/avs/imagecapturer/controller/ImageCapturerController.java
+++ AVS_ImageCapturer/src/de/fh_koeln/avs/imagecapturer/controller/ImageCapturerController.java
@@ -1,5 +1,6 @@
 package de.fh_koeln.avs.imagecapturer.controller;
 
+import de.fh_koeln.avs.imagecapturer.converter.MatToBufferedImageConverter;
 import java.awt.image.BufferedImage;
 import org.opencv.core.Mat;
 import org.opencv.highgui.VideoCapture;
@@ -10,30 +11,42 @@ import org.opencv.highgui.VideoCapture;
  */
 public class ImageCapturerController implements IImageCapturerController {
 
-    private VideoCapture camera;
-    private Mat capturedImage;
+    private final VideoCapture camera;
+    private final MatToBufferedImageConverter matToBufferedImageConverter;
+    private BufferedImage capturedBufferedImage;
 
     public ImageCapturerController() {
-    
-    }
-    
-    @Override
-    public void startCamera() {
-        if (camera == null) {
-            camera = new VideoCapture(0);
-        }
-    }
-    
-    @Override
-    public void stopCamera() {
-        if(camera != null) {
-            camera.release();
-        }
-    }
-    
-    @Override
-    public BufferedImage getCapturedImage() {
-        return null;
+        camera = new VideoCapture();
+        matToBufferedImageConverter = new MatToBufferedImageConverter();
     }
 
+    @Override
+    public void startCamera() {
+        if (camera.open(0)) {
+            System.out.println("Camera started");
+        } else {
+            System.out.println("Couldn't start camera");
+        }
+    }
+
+    @Override
+    public void stopCamera() {
+        camera.release();
+        if (!camera.isOpened()) {
+            System.out.println("Camera stopped");
+        } else {
+            System.out.println("Couldn't stop camera");
+        }
+    }
+
+    @Override
+    public BufferedImage getCapturedImage() {
+        if (camera.isOpened()) {
+            Mat image = null;
+            camera.read(image);
+            capturedBufferedImage
+                    = matToBufferedImageConverter.convertToBufferedImage(image);
+        }
+        return capturedBufferedImage;
+    }
 }
---
+++ AVS_ImageCapturer/src/de/fh_koeln/avs/imagecapturer/converter/MatToBufferedImageConverter.java
@@ -0,0 +1,15 @@
+package de.fh_koeln.avs.imagecapturer.converter;
+
+import java.awt.image.BufferedImage;
+import org.opencv.core.Mat;
+
+/**
+ *
+ * @author Daniel van der Wal
+ */
+public class MatToBufferedImageConverter {
+
+    public BufferedImage convertToBufferedImage(Mat matImage) {
+        return null;
+    }
+}
