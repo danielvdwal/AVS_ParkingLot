8a97e198d6e18fc8804d01b11a145d57edcb3187
Daniel van der Wal <dvanderw@pip07.inf.fh-koeln.de>
2014-08-08 13:42:25 +0200

    added opencv libs as jar to project

--- AVS_ImageCapturer/src/de/fh_koeln/avs/imagecapturer/Main.java
+++ AVS_ImageCapturer/src/de/fh_koeln/avs/imagecapturer/Main.java
@@ -1,9 +1,47 @@
 package de.fh_koeln.avs.imagecapturer;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+
 /**
  *
  * @author Daniel van der Wal
  */
 public class Main {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Hello, OpenCV");
+    // Load the native library.
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+    VideoCapture camera = new VideoCapture(0);
+    camera.open(0); //Useless
+    if(!camera.isOpened()){
+        System.out.println("Camera Error");
+    }
+    else{
+        System.out.println("Camera OK?");
+    }
+
+    Mat frame = new Mat();
+
+    //camera.grab();
+    //System.out.println("Frame Grabbed");
+    //camera.retrieve(frame);
+    //System.out.println("Frame Decoded");
+
+    camera.read(frame);
+    System.out.println("Frame Obtained");
+
+    /* No difference
+    camera.release();
+    */
+
+    System.out.println("Captured Frame Width " + frame.width());
+
+    Highgui.imwrite("camera.jpg", frame);
+    System.out.println("OK");
+    }
 }
